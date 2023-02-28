824c9224f8b971ebd5eb88d7d46b6eb7176f6792
Selina Su <phongsu20@gmail.com>
2023-02-27 19:59:38 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+function App() {
+  const expenses = [
+    {
+      title: 'Toilet Paper', 
+      amount: 94.12, 
+      date: new Date(2020, 7, 14)
+    },
+    {
+      title: 'New TV', 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12)
+    },
+    {
+      title: 'Car Insurance', 
+      amount: 294.67, 
+      date: new Date(2021, 2, 28)
+    },
+    {
+      title: 'New Desk (Wooden)', 
+      amount: 450, 
+      date: new Date(2021, 5, 12)
+    },
+  ];
+
+  const addExpenseHandler = expense => {
+    console.log(expenses);
+  };
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses items={expenses} />
+    </div>
+  );
+}
+
+export default App;
